6a87d536223775ad84213d3e9d630775a0016b76
Martin <mheberling@microsoft.com>
2024-08-13 15:15:38 -0700

    removed azuresdk flag from server.

--- server/cmd/server/start.go
+++ server/cmd/server/start.go
@@ -20,7 +20,6 @@ func init() {
 	startCmd.Flags().IntVar(&options.Port, "port", 50051, "the addr to serve the api on")
 	startCmd.Flags().BoolVar(&options.JsonLog, "json-log", false, "The format of the log is json or user friendly key-value pairs")
 	startCmd.Flags().StringVar(&options.SubscriptionID, "subscription-id", "", "The subscription ID to connect to")
-	startCmd.Flags().BoolVar(&options.EnableAzureSDKCalls, "enable-azureSDK-calls", false, "Toggle to run azureSDK CRUDL calls if cluster is enabled with workload-id")
 	startCmd.Flags().IntVar(&options.HTTPPort, "http port", 50061, "the addr to serve the gRPC-Gateway on")
 	startCmd.Flags().StringVar(&options.RemoteAddr, "remote-addr", "", "the demo server's addr for this server to connect to")
 	startCmd.Flags().Int64Var(&options.IntervalMilliSec, "interval-milli-sec", options.IntervalMilliSec,
